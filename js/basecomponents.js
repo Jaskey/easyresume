e0714cb194f81e560806e1cccef3c2dbccd0739b
ljjn1246 <ljjn1246@corp.netease.com>
2016-04-18 16:50:11 +0800

    repo init

---
+++ js/basecomponents.js
@@ -0,0 +1,97 @@
+﻿		var Fragment = React.createClass({
+			getDefaultProps:function(){
+				return {
+					option:{
+						name:'',
+						comment:'',
+						startDate:'',
+						endDate:'',
+						summary:'',
+						detail:[]
+					}
+				}
+			},
+			render:function(){
+						var frag = this.props.option;
+						
+						if(frag.startDate&&frag.endDate){
+							var _dateComp =<div className="date">
+										<span>{frag.startDate}</span>
+										<span>-</span>
+										<span>{frag.endDate}</span>
+									  </div>
+						}
+						
+						if(frag.summary){
+							var _summaryComp =
+									<div className="fragment-summary">
+										<div>{frag.summary}</div>
+									</div>
+						}
+						
+						return (
+							<div className="fragment">
+								<div className="circle-point"></div>
+								<div className="fragment-content-wrapper">
+									<span className="fragment-name">{frag.name}</span>			
+									<span className="fragment-comment">{frag.comment}</span>			
+									<div className="fragment-content">
+										{_summaryComp}
+										<div className="fragment-list">
+											<ul>
+												{frag.detail.map(function(e,i){return <li key={"frament_"+i}>{e}</li>})}
+											</ul>
+										</div>
+										
+										{_dateComp}
+										
+									</div>
+								</div>
+							</div>
+						)
+			}
+		});
+
+
+		/**
+		一整块区域，一个section下面每一段子区域为Fragment
+		**/
+		var Section = React.createClass({
+			getDefaultProps:function(){
+				return {
+					title:"sectionTitle",
+					fragments:[],
+					points:[]
+				}
+			},
+			
+			render:function(){
+				var cSection;
+				if(this.props.fragments.length>0){
+					var frags=[];
+					for(var i=0;i<this.props.fragments.length;i++){
+						var frag = this.props.fragments[i];
+						var fragComponent=<Fragment key={"fragment_"+i} option={frag}/>//一个Fragment component
+						frags.push(fragComponent);
+					}
+					var actualEle = frags;
+					cSection = <div className="timeline-section-content">{actualEle}</div>
+				}else if(this.props.points.length>0){
+					var liElement = [];
+					for(var i=0;i<this.props.points.length;i++){
+						var point = this.props.points[i];
+						liElement.push(<li key={"point_"+i}>{point}</li>);
+					}
+					var pointEle=<div className="points-container"><ul>{liElement}</ul></div>;
+					var actualEle = pointEle;
+					cSection = <div className="section-content">{actualEle}</div>
+				}
+				return (
+							<div className="section">
+								<h2 className="section-title">{this.props.title}</h2>
+								{cSection}
+							</div>
+					)
+			}
+			
+		});
---
+++ js/resumecomponents.js
@@ -0,0 +1,373 @@
+﻿		var Resume = React.createClass({
+			getDefaultProps:function (){
+				console.log('[Resume][getDefaultProps]');
+				return {
+					json:{}
+				}
+			},
+			
+
+			getInitialState:function(){
+				console.log('[Resume][getInitialState]');
+				return {
+					
+				}
+			},
+
+			render:function(){
+
+				var cSumary = <Summary key="summary-section" summary = {this.props.json.summary}/>;//概览模块
+				var cWork = <Work key="work-section" workExperience = {this.props.json.workExperience}/>//工作模块
+				var cProject = <ProjectExperience key="project-section" projectExperience = {this.props.json.projectExperience}/>//项目经验
+				var cOtherSection = <OtherSections key="other-section" sections={this.props.json.sections}/>//其他模块
+				var cCourse = <Course key="course-section" courses={this.props.json.courses}/>//课程
+				var cEdu = <Educations key="education-section" educations={this.props.json.educations}/>;//教育经历
+				
+				var comps=[cSumary,cWork,cProject,cOtherSection,cCourse,cEdu];//组合起来成为一个大的component
+				
+				return (
+					<div id="mainContent">
+						<Info name={this.props.json.name} //姓名
+							  birth={this.props.json.birth}//生日
+							  email={this.props.json.email}//邮件
+							  tel={this.props.json.tel}//电话
+							  avatar={this.props.json.avatar}//头像
+							  pages={this.props.json.pages}//个人主页的icon链接
+							  />
+						<div id="resumeContent">
+							{/*简历主内容*/}
+							{comps}
+						</div>
+						<div id="footer"><div id="credit">Powered by <a href="#">easyresume</a></div></div>{/*水印*/}
+					</div>
+				)
+			},
+			
+			componentDidMount:function(){
+				console.log('[Resume][componentDidMount]');
+			}
+		})
+		
+		var PageIcon=React.createClass(		
+			function(){
+				var iconfontKeymap = {
+						"weixin": "&#xe603;",
+						"zhihu": "&#xe607;",
+						"douban": "&#xe605;",
+						"github": "&#xe604;",
+						"weibo": "&#xe601;",
+						"stackoverflow": "&#xe600;",
+						"segmentfault": "&#xe602;",
+						"lofter": "&#xe608;",
+						"linkedin": "&#xe606;"
+				};
+				return {
+					getDefaultProps:function(){
+						return {
+							page:{}
+						}
+					},
+
+
+					render:function(){
+						var pageUrl;
+						for( var key in this.props.page){
+							pageUrl=this.props.page[key];
+							break;
+						}
+						return (<a ref="iconFontSpan" className="page-icon-font" href={pageUrl} target="_blank"></a>)
+					},
+					componentDidMount:function(){
+						var page="default";
+						
+						for( var key in this.props.page){
+							page=key.toLowerCase();
+							break;
+						}
+						
+						//console.log("page=",page," icon-font:",iconfontKeymap[page]);
+						//console.log(React.findDOMNode(this.refs.iconFontSpan));
+						$(React.findDOMNode(this.refs.iconFontSpan)).html(iconfontKeymap[page]);
+						}
+				}
+			}());
+		
+		var Info  = React.createClass({
+			getDefaultProps:function(){
+				return {
+					name:null,
+					birth:null,
+					tel:null,
+					email:null,
+					avatar:'/image/avatar.jpg',
+					pages:[]
+				}
+			},
+			render:function(){
+				var aPages=[];
+				for(var i = 0;i<this.props.pages.length;i++){
+					var cPage = <PageIcon key={"page-icon"+i} page={this.props.pages[i]}/> ;	
+					aPages.push(cPage);
+				}
+				return (
+					<div className="info" >
+						<div className="info-brief" >
+							<div className="avatar"><img src={this.props.avatar}/></div>
+							<div className="name">{this.props.name}</div>
+							<div className="birth" ref="birth">{this.props.birth}</div>
+							<div className="pages-link">
+								{aPages}
+							</div>
+						</div>
+						<div className="info-contact">
+								<div>电话:{this.props.tel}</div>
+								<div>邮件:{this.props.email}</div>
+						</div>
+					</div>
+				)
+			},
+		});
+		
+		var Summary = React.createClass({
+			getDefaultProps:function(){
+				return {
+					summary:[]
+				}
+			},			
+			render:function(){
+				var summary = this.props.summary;
+				var lis = [];
+				for(var i=0;i<summary.length;i++){
+					lis.push(<li key={"summary-paragrph"+i}>{summary[i]}</li>);
+				};
+
+				return (
+								<div className="summary section">
+									<h2 className="section-title">概述</h2>
+									<div className="section-content">
+										<ul>
+											{lis}
+										</ul>
+									</div>
+								</div>
+						)
+						
+			}
+				
+		});
+		
+		
+		var Work = React.createClass({
+			getDefaultProps:function(){
+				workExperience:[]
+			},
+			
+			render:function(){
+				var ex=[];//工作经历
+				var workExperience = this.props.workExperience;
+				
+				for(var i =0;i<workExperience.length;i++){
+					var we = workExperience[i]
+					var fragmentOption={
+						name:we.company,
+						comment:we.post,
+						startDate:we.startDate,
+						endDate:we.endDate,
+						summary:we.summary,
+						detail:we.detail
+					};
+					//<Fragment option={fragmentOption}/>
+					//console.log(we,"companyLogo: ",we.companyLogo)
+					var work = 
+								<div key={"work-experience_"+i} className="work-Fragment">
+									<div className="work-date">
+										<div>{we.startDate}</div>
+										<div className="date-separator"></div>
+										<div>{we.endDate} </div>
+									</div>
+									
+									<div className="companyLogo circle-point">
+										{we.company[0]}
+									</div>
+										
+									<div className="work-content">
+										<h2>{we.company}</h2>
+										<h3>{we.post}</h3>
+										
+										<div className="work-detail">
+											<ul>
+												{
+													we.detail.map(function(e,i){return <li key={"work-detail_"+we.company+"_"+we.post+i}>{e}</li>})
+												}
+											</ul>
+										</div>
+									</div>
+								</div>;
+
+					
+					ex.push(work);
+				}
+			
+				return(
+					<div className="section work-experience">	
+						<h3 className="section-title">工作经历</h3>
+						<div className="section-content">{ex}</div>
+					</div>
+				)
+			}
+			
+		});
+		
+		
+		var ProjectExperience = React.createClass({
+			getDefaultProps:function(){
+				projectExperience:[]
+			},
+			
+			render:function(){
+			
+				var ex=[];
+				
+				var projectExperience = this.props.projectExperience;
+				for(var i =0;i<projectExperience.length;i++){
+					var pe = projectExperience[i]
+					var fragmentOption={
+						name:pe.projectName,
+						comment:pe.role,
+						startDate:pe.startDate,
+						endDate:pe.endDate,
+						summary:pe.summary,
+						detail:pe.detail
+					}		
+					
+					var project = 
+						<div key={"project-experience_"+i} className="project-Fragment">
+							<Fragment option={fragmentOption}/>
+						</div>;
+					ex.push(project);
+				}
+			
+				return (
+					<div className=" section project-experience">
+						<h3 className="section-title">项目经历</h3>
+						<div className="timeline-section-content">{ex}</div>
+					</div>
+				)
+			}
+		});
+		
+		
+
+		
+		var OtherSections = React.createClass({
+			getDefaultProps:function(){
+				return {
+					sections:[]
+				}
+			},
+			
+			render:function(){
+				var secComponents=[];//所有section 的components数组
+				
+				for(var i=0;i<this.props.sections.length;i++){
+					var sec = this.props.sections[i];//一个section配置
+					var secComponent=<Section key={"other-section_"+i} title={sec.title} fragments = {sec.fragments} points = {sec.points}/>//一个section component
+					secComponents.push(secComponent);
+				}
+				return (
+							<div className="common-sections">
+								{secComponents}
+							</div>
+					)
+			}
+			
+		});
+		
+		var Course  =  React.createClass({
+			getDefaultProps:function(){
+				return {
+					courses:[]
+				}
+			},
+			
+			render:function(){
+				var courses = this.props.courses;
+				var liElements = [];
+				for(var i =0;i<courses.length;i++){
+					var course = courses[i];
+					var li=<div><span>{course.courseName}</span><span>{course.courseProvider}</span></div>
+					liElements.push(li);
+				}
+			
+				return (
+					
+					liElements.length>0?
+						<div className="section course">
+							<h2 className="section-title">MOOC证书</h2> 
+							<div className="section-content">{liElements}</div>
+						</div>
+						:<div></div>
+				)
+			}
+		});
+		
+		var Educations = React.createClass({
+		
+			getDefaultProps:function(){
+				return {
+					educations:[{
+						university:'',
+						studyType:'',
+						major:'',
+						startDate:'',
+						endDate:'',
+						gpa:'',
+						scholarships:[],
+						experiences:[]
+					}]
+				}
+			}
+			,
+			render:function(){
+				var educations = this.props.educations;
+				var eduEles = [];//教育经历的内容
+				for(var i=0;i<educations.length;i++){
+					var edu = educations[i];
+					//在校实践
+					var exe = edu.experiences&&edu.experiences.length>0?
+								<div className="education-exeperience">
+									<ul>
+										{edu.experiences.map(function(e,index){return <li key={"education-exeperience"+i+"_"+index}>{e}</li>})}
+									</ul>
+								</div>:'';
+					
+					var section = 					
+						<div key={"education_section-content_"+i} className="section-content">
+								<div className="university">{edu.university}</div>								
+								<div className="major"><span className="major-name">{edu.major}</span>|<span className="study-type">{edu.studyType}</span></div>
+								<div className="date">
+										<span>{edu.startDate}</span>
+										<span>-	</span>
+										<span>{edu.endDate}</span>
+								</div>
+								
+								<div className="edu-content">
+									<div className="score">
+										<div className="gpa"><span>GPA:{edu.gpa}</span></div>
+										<div className="scholarship"><span>奖学金:{edu.scholarships.join(' ')}</span></div>
+									</div>
+									{exe}
+								</div>
+
+						</div>
+					
+					eduEles.push(section);
+				}
+				return (
+					<section className="section education-section">
+						<h3 className="section-title">教育经历</h3>
+						{eduEles}
+					</section>
+				)
+			}
+		})
